52d59ecd3810bb204636cb48be030936d3533ee8
Deniz Erdem <derdemm7@gmail.com>
2023-07-18 00:26:58 +0300

    form scripts

--- app/src/app/home/home.component.ts
+++ app/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  title: string = '';
+  options: string[] = ['', ''];
+
+  constructor(private elementRef: ElementRef) { }
+
+  checkOptionInput(event: any, i: number) {
+    console.log(event.target.value + '\n' + i);
+    this.checkOption(event, i)
+  }
+  checkOptionBlur(event: any, i: number) {
+    if (event.target.value == '' && this.options.length > 1 && i != this.options.length - 1) {
+      console.log('empty and not last');
+      this.options.splice(i, 1);
+      return;
+    }
+    this.checkOption(event, i)
+  }
+
+  checkOption(event: any, i: number) {
+    if (event.target.value != '' && i >= 1 && i == this.options.length - 1) {
+      console.log('not empty and last');
+      this.options.push('');
+    }
+  }
+
+  focusPin(event: any, i: number) {
+    if (event.target.value == '') {
+      console.log('empty');
+      if (i != 0) {
+        this.elementRef.nativeElement.querySelector(`#pin${i - 1}`).focus();
+      }
+      return;
+    }
+    this.elementRef.nativeElement.querySelector(`#pin${i + 1}`).focus();
+  }
+
+  deny() {
+    console.log(this.options);
+  }
 
 }
