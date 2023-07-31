280516fd2cebfe2d0984200c82bd4c7274acdc29
Deniz Erdem <derdemm7@gmail.com>
2023-08-01 00:44:23 +0300

    poll comp ui

--- app/src/app/poll/poll.component.ts
+++ app/src/app/poll/poll.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-poll',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./poll.component.css']
 })
 export class PollComponent {
+  constructor(private route: ActivatedRoute) { }
+  id: number = 0;
+  poll = {
+    id: 123456,
+    title: 'What is your favorite color?',
+    options: [
+      { name: 'Red', votes: 89 },
+      { name: 'Green', votes: 56 },
+      { name: 'Blue', votes: 34 },
+      { name: 'Yellow', votes: 45 },
+      { name: 'Orange', votes: 56 },
+      { name: 'Purple', votes: 13 }
+    ],
+    totalVotes: 293
+  }
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.id = params['id'];
+    });
+  }
+  selectedOption: number | null = null;
 
+  selectOption(index: number) {
+      this.selectedOption = index;
+  }
 }
+
